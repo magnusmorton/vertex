279bc70c4eb1528c07a5d1f83a5e4f5c2a795fd0
Magnus Morton <magnus@morton.ai>
2021-03-29 20:13:12 +0100

    inserting calls to runtime

--- GraphSan.cc
+++ GraphSan.cc
@@ -1,20 +1,47 @@
 #include "llvm/IR/PassManager.h"
+#include "llvm/IR/IRBuilder.h"
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/Passes/PassPlugin.h"
 #include "llvm/Support/raw_ostream.h"
 #include <iostream>
+#include <vector>
 
 #include "GraphSan.h"
 
 using namespace llvm;
 
+FunctionCallee create_func;
+
 PreservedAnalyses GraphPass::run(Function &F, FunctionAnalysisManager &M) {
+  IRBuilder<> builder(F.getContext());
+  outs() << "run on function\n";
   visit(F);
   return PreservedAnalyses::all();
 };
 
-PreservedAnalyses GraphPass::run(Module &F, ModuleAnalysisManager &M) {
-  visit(F);
+PreservedAnalyses GraphPass::run(Module &M, ModuleAnalysisManager &MAM) {
+
+  LLVMContext &ctx = M.getContext();
+  /*
+    dfsan mangles function symbols on instrumented CUs. Our pass is run after
+    this happens. I can't find a way of running it before with the new pass
+    manager, so I do it here. 
+  */
+  create_func = M.getOrInsertFunction(
+    "dfs$_create_label",
+    Type::getVoidTy(ctx),
+    Type::getInt8PtrTy(ctx),
+    Type::getInt8PtrTy(ctx),
+    Type::getInt64Ty(ctx)
+    );
+  
+  FunctionCallee check_fun = M.getOrInsertFunction(
+    "_check_ptr",
+    Type::getVoidTy(ctx),
+    Type::getInt8PtrTy(ctx)
+    );
+  
+  visit(M);
   return PreservedAnalyses::none();
 };
 
@@ -23,21 +50,33 @@ void GraphPass::visitCallInst(CallInst &callinst) {
   StringRef name = "none";
   if (func)
     name = func->getName();
-  outs() << name << "\n";
+
+  if (name == "malloc") {
+    printf("malloccccccc!!!!!!!\n");
+    IRBuilder<> builder(callinst.getNextNode());
+    std::vector<Value*> args;
+    Function *func = callinst.getFunction()->getParent()->getFunction("dfs$_create_label");
+    assert(func);
+    Value *strptr = builder.CreateGlobalStringPtr("labbbellll");
+    args.push_back(strptr);
+    args.push_back(&callinst);
+    for (auto& arg : callinst.args()) {
+      std::cout  << typeid(arg).name() << "\n";
+      std::cout  << arg.get() << "\n";
+      args.push_back(arg.get());
+      outs() << callinst << "\n";
+    }
+    builder.CreateCall(func, args);
+  }
 }
 
 void GraphPass::visitAllocaInst(AllocaInst &allInst) {
-  std::cout << "foooooooooooooooooooooooooooooo"
-            << "\n";
   outs() << allInst << "\n";
 }
 
 extern "C" ::llvm::PassPluginLibraryInfo LLVM_ATTRIBUTE_WEAK
 llvmGetPassPluginInfo() {
   return {LLVM_PLUGIN_API_VERSION, "GraphPass", "v0.1", [](PassBuilder &PB) {
-            std::cout << "gjasdlk;jaskld;fjklsdajkl;fjaslkd;f"
-                      << "\n";
-
             PB.registerPipelineStartEPCallback(
                 [](llvm::ModulePassManager &PM) { PM.addPass(GraphPass()); });
           }};
