7abd0e243c246db280f153ed8927311a48581eaf
Magnus Morton <magnus@morton.ai>
2021-06-09 15:20:43 +0100

    changed from dfsan_set_label to dfsan_add_label

--- examples/df_simple.c
+++ examples/df_simple.c
@@ -8,18 +8,20 @@ int main(void) {
   int a[128], b[128];
   memset(a, 7, 128 * sizeof(int));
   dfsan_label a_label = dfsan_create_label("a", 0);
-  dfsan_set_label(a_label, a, sizeof(a));
+  dfsan_add_label(a_label, a, sizeof(a));
   dfsan_label b_label = dfsan_create_label("b", 0);
-  dfsan_set_label(b_label, b, sizeof(b));
+  dfsan_add_label(b_label, b, sizeof(b));
   int tmp = 0;
   // init b
+  dfsan_label check = dfsan_read_label(b+2, sizeof(int));
+  assert(dfsan_has_label(check,b_label));
   for (int i=0; i < 128; i++) {
     b[i] = i;
   }
   for (int i=0; i < 128; i++) {
     tmp += a[b[i]];
-    dfsan_label check = dfsan_read_label(&tmp, sizeof(tmp));
-    assert(dfsan_has_label(check,a_label));
+    check = dfsan_read_label(b, sizeof(b));
+//    assert(dfsan_has_label(check,a_label));
     assert(dfsan_has_label(check,b_label));
   }
   printf("tmp is %d\n", tmp);
