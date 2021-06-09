9f4efdb181a24cf88c15d4d388fcc61956f837b0
Magnus Morton <magnus@morton.ai>
2021-06-09 15:17:01 +0100

    successful demo of --dfsan-combine-pointer-labels-on-store

---
+++ examples/real_simple.c
@@ -0,0 +1,31 @@
+#include <sanitizer/dfsan_interface.h>
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void) {
+  int a;
+  int tmp = 6;
+  int *b = &tmp;
+  a = 9;
+  dfsan_label a_label = dfsan_create_label("a", 0);
+  dfsan_add_label(a_label, &a, sizeof(a));
+  dfsan_label b_label = dfsan_create_label("b", 0);
+  dfsan_add_label(b_label, &b, sizeof(b));
+  printf("b label %hd\n", b_label);
+  dfsan_label check = dfsan_read_label(&b, sizeof(b));
+  printf("check label %hd\n", check);
+  assert(dfsan_has_label(check,b_label));
+  *b = a;
+  check = dfsan_read_label(&b, sizeof(int));
+  printf("check label %hd\n", check);
+  assert(dfsan_has_label(check,b_label));
+
+  check = dfsan_read_label(b, sizeof(int));
+  assert(dfsan_has_label(check,a_label));
+  assert(dfsan_has_label(check,b_label));
+  
+
+  return 0;
+}
