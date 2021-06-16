0830593d034df102ac0f0888769847d69e9ad807
Magnus Morton <magnus@morton.ai>
2021-06-16 22:05:04 +0100

    dumping graphviz

--- runtime.cc
+++ runtime.cc
@@ -16,24 +16,22 @@
    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 #include <algorithm>
+#include <fstream>
 #include <iostream>
 #include <functional>
 #include <vector>
 
 #include <boost/graph/graph_traits.hpp>
 #include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/graphviz.hpp>
 
 #include <limits.h>
+#include <stdlib.h>
 
 #include "runtime.h"
 
-const int base_size = 7;
 
-static int inited = 0;
-
-
-
-
+bool inited = false;
 
 struct memory_node {
   char *start;
@@ -43,32 +41,50 @@ struct memory_node {
 std::vector<memory_node> roots;
 
 using namespace boost;
-typedef adjacency_list<listS,setS,directedS> MemGraph;
+
+typedef adjacency_list<vecS,vecS,directedS> MemGraph;
+std::vector<MemGraph::vertex_descriptor> vds;
 MemGraph memGraph;
 
+void finish() {
+  std::ofstream file;
+  file.open("graph.dot");
+  write_graphviz(file, memGraph);
+  file.close();
+}
+
 extern "C" void _mark_root(const char* label, char *ptr, size_t size, const char* file, unsigned line) {
+  if (!inited) {
+    atexit(&finish);
+    inited = true;
+  }
   printf("ROOT at ptr %p, extent %lu, label %s, file %s:%d\n", ptr, size, label, file, line);
   struct memory_node nd = {ptr, ptr + size};
   roots.push_back(nd);
-  
+  vds.push_back(add_vertex(memGraph));
 }
 
 extern "C" void _check_ptr(char *ptr, const char *file, unsigned line) {
-  std::cout << "doing nothing\n";
+  std::cerr << "doing nothing\n";
 }
 
 std::function<bool(memory_node&)> find_func(char *ptr) {
   return [ptr](memory_node &node) { return ptr >= node.start && ptr <= node.end; };
 }
+
 extern "C" void _handle_store(char *target, char *source) {
   printf("handling store.....\n");
   auto ti = std::find_if(roots.begin(), roots.end(), find_func(target));
   auto si = std::find_if(roots.begin(), roots.end(), find_func(source));
 
   if (ti == roots.end() || si == roots.end())
-    std::cout << "target or source not found\n";
-  else
+    std::cerr << "target or source not found\n";
+  else {
     printf("source %ld, sink %ld\n", si-roots.begin(), ti-roots.begin());
+    auto svd = vds[si-roots.begin()];
+    auto tvd = vds[ti-roots.begin()];
+    add_edge(svd, tvd, memGraph);
+  }
 }
 
 
