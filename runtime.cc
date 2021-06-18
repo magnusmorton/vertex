70da6e0dddbeb2d2fae927ade1f38b427fba345a
Magnus Morton <magnus@morton.ai>
2021-06-18 19:08:17 +0100

    first go at removing old edges

--- runtime.cc
+++ runtime.cc
@@ -21,6 +21,7 @@
 #include <functional>
 #include <vector>
 
+#include <boost/bimap.hpp>
 #include <boost/graph/graph_traits.hpp>
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/graphviz.hpp>
@@ -40,11 +41,16 @@ struct memory_node {
 
 std::vector<memory_node> roots;
 
+
+
+
 using namespace boost;
 
 typedef adjacency_list<vecS,vecS,directedS> MemGraph;
 std::vector<MemGraph::vertex_descriptor> vds;
 MemGraph memGraph;
+std::map<char*, MemGraph::edge_descriptor> prev_stores;
+std::map<MemGraph::edge_descriptor, unsigned> edge_refs;
 
 void finish() {
   std::ofstream file;
@@ -83,7 +89,30 @@ extern "C" void _handle_store(char *target, char *source) {
     printf("source %ld, sink %ld\n", si-roots.begin(), ti-roots.begin());
     auto svd = vds[si-roots.begin()];
     auto tvd = vds[ti-roots.begin()];
-    add_edge(svd, tvd, memGraph);
+    auto[edge, added] = add_edge(svd, tvd, memGraph);
+    auto prev_edge_it = prev_stores.find(target);
+    if (prev_edge_it != prev_stores.end()) {
+      std::cerr << "previous store\n";
+      auto prev_edge = prev_edge_it->second;
+      // probably unecessary
+      if (edge_refs[prev_edge] > 0) {
+        edge_refs[prev_edge]--;
+        if (edge_refs[prev_edge] == 0)
+          remove_edge(prev_edge, memGraph);
+      }
+    } else {
+      std::cerr << "new store\n";
+      prev_stores[target] = edge;
+      auto edge_it = edge_refs.find(edge);
+      // If edge already exists between these blocks, incremenet refcount
+      // otherwise, create ref
+      if (edge_it != edge_refs.end()) {
+        edge_it->second++;
+      } else {
+        edge_refs[edge] = 1;
+      }
+    }
+    
   }
 }
 
