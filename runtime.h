1ba75c9d038e4127347149807f1ed973791e1fa4
Magnus Morton <magnus@morton.ai>
2021-06-23 17:12:06 +0100

    removed uncessary exports

--- runtime.h
+++ runtime.h
@@ -1,15 +1,20 @@
 #include <stdlib.h>
-#ifdef __cplusplus
-extern "C"
-#endif
-void _mark_root(const char *, char*, size_t, const char*, unsigned);
+
+/* #ifdef __cplusplus */
+/* extern "C" */
+/* #endif */
+/* void _mark_root(const char *, char*, size_t, const char*, unsigned); */
+
+/* #ifdef __cplusplus */
+/* extern "C" */
+/* #endif */
+/* void _check_ptr(char *, const char*, unsigned); */
+
+/* #ifdef __cplusplus */
+/* extern "C" */
+/* #endif */
+/* void _handle_store(char *target, char *source); */
 
 #ifdef __cplusplus
 extern "C"
 #endif
-void _check_ptr(char *, const char*, unsigned);
-
-#ifdef __cplusplus
-extern "C"
-#endif
-void _handle_store(char *target, char *source);
