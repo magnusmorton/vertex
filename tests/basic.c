cb24027078b387c6a92b0fcfdc1a2a026bc1d093
Magnus Morton <magnus@morton.ai>
2021-07-13 17:41:19 +0100

    using test fixtures

--- tests/basic.c
+++ tests/basic.c
@@ -4,26 +4,48 @@
 
 extern void _mark_root(const char* label, void *ptr, size_t size, const char* file, unsigned line);
 
-static void test_no_data() {
+
+
+static void
+set_up_fixture(GArray **array,
+               gconstpointer data)
+{
+  /* do nothing */
+  *array = NULL;
+}
+
+static void
+tear_down_fixture(GArray **array,
+                  gconstpointer data)
+{
+  g_array_free(*array, TRUE);
+}
+
+static void
+test_no_data(GArray **detected,
+             gconstpointer data)
+{
   // do nothing
 
-  GArray *detected = get_detected();
-  g_assert_true(detected->len ==0);
-  g_array_free(detected, TRUE);
+  *detected = get_detected();
+  g_assert_true((*detected)->len == 0);
 }
 
-static void test_1_data() {
+static void
+test_1_data(GArray **detected,
+            gconstpointer data)
+{
   void *ptr = 0x94;
   _mark_root("foo", ptr, 8, NULL, 0);
-  GArray *detected = get_detected();
-  g_assert_true(detected->len == 1);
-  g_array_free(detected, TRUE);
+  *detected = get_detected();
+  g_assert_true((*detected)->len == 1);
 }
-
 
 int main(int argc, char *argv[]) {
   g_test_init (&argc, &argv, NULL);
-  g_test_add_func("/basic/no_data", test_no_data);
-  g_test_add_func("/basic/one_data", test_1_data);
+  g_test_add("/basic/no_data", GArray*, NULL,
+             set_up_fixture, test_no_data, tear_down_fixture);
+  g_test_add("/basic/one_data", GArray*, NULL,
+                  set_up_fixture, test_1_data, tear_down_fixture);
   return g_test_run();
 }
