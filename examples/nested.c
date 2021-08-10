42b73fa6cba8a464570a113907be5d6e59bb7a48
Magnus Morton <magnus@morton.ai>
2021-08-10 16:45:53 +0100

    nested example complete

--- examples/nested.c
+++ examples/nested.c
@@ -7,25 +7,46 @@ typedef struct link {
 } link;
 
 
-void insert_append (link *anchor, link *newlink) {
+void
+insert_append (link *anchor, link *newlink)
+{
   newlink->next = anchor->next;
   anchor->next = newlink;
 }
 
-link *link_new(int data) {
+link*
+link_new(long data)
+{
+  link *tmp = malloc(sizeof(link));
+  tmp->next = NULL;
+  tmp->data = (void *)data;
+  return tmp;
+}
+
+link*
+dlink_new(void *data)
+{
   link *tmp = malloc(sizeof(link));
   tmp->next = NULL;
   tmp->data = data;
   return tmp;
 }
 
-int main() {
-  link *a, *b, *c;
+int 
+main()
+{
+  link *a, *b, *c, *d, *e, *out_a, *out_b;
   a = link_new(1);
   b = link_new(2);
   c = link_new(3);
-  insert_append (a, b);
-  insert_append (a, c);
+  d = link_new(4);
+  e = link_new(5);
+  insert_append(d, e);
+  insert_append(a, b);
+  insert_append(a, c);
+  out_a = dlink_new(a);
+  out_b = dlink_new(d);
+  insert_append(out_a, out_b);
   free(a);
   free(b);
   free(c);
