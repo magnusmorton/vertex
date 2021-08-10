2ff6960473a0642f1c4b2a58a5455715c2ac3a83
Magnus Morton <magnus@morton.ai>
2021-08-10 11:09:30 +0100

    nested linked list example added

---
+++ examples/nested.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct link {
+  struct link *next;
+  void *data;
+} link;
+
+
+void insert_append (link *anchor, link *newlink) {
+  newlink->next = anchor->next;
+  anchor->next = newlink;
+}
+
+link *link_new(int data) {
+  link *tmp = malloc(sizeof(link));
+  tmp->next = NULL;
+  tmp->data = data;
+  return tmp;
+}
+
+int main() {
+  link *a, *b, *c;
+  a = link_new(1);
+  b = link_new(2);
+  c = link_new(3);
+  insert_append (a, b);
+  insert_append (a, c);
+  free(a);
+  free(b);
+  free(c);
+}
